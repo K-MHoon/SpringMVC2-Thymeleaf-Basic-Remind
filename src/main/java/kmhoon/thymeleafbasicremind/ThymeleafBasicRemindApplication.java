0353a8143268b4e57e9312a4dc0cffbcbdc17e46
K-MHoon <azmria117@gmail.com>
2021-12-01 08:44:02 +0900

    Initial commit

---
+++ src/main/java/kmhoon/thymeleafbasicremind/ThymeleafBasicRemindApplication.java
@@ -0,0 +1,13 @@
+package kmhoon.thymeleafbasicremind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafBasicRemindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafBasicRemindApplication.class, args);
+	}
+
+}
