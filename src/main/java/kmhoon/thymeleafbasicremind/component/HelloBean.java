d42877695907dee0669b8e692d6199bcfc05465d
K-MHoon <azmria117@gmail.com>
2021-12-01 15:34:09 +0900

    feat : 타임리프 기본 문법들
    - 텍스트, 변수(SpringEL), 기본 객체들
    - 유틸리티 객체, 날짜, URL 링크
    - 리터럴, 연산, 속성 값 설정
    - 반복, 조건부 평가, 주석, 블록
    - 자바스크립트 인라인

---
+++ src/main/java/kmhoon/thymeleafbasicremind/component/HelloBean.java
@@ -0,0 +1,10 @@
+package kmhoon.thymeleafbasicremind.component;
+
+import org.springframework.stereotype.Component;
+
+@Component("helloBean")
+public class HelloBean {
+    public String hello(String data) {
+        return "Hello " + data;
+    }
+}
